86018a0c0a312fbda39c7e8965f0fcbc16596bf0
Guilherme Gouveia <gouveiagsilva@gmail.com>
2025-07-18 21:58:18 -0300

    FIX: remove CONSTRAINTS complexas do script de criação de tabelas

--- Dados/mySQL/Dev/Scripts/01_criacao_tabelas.sql
+++ Dados/mySQL/Dev/Scripts/01_criacao_tabelas.sql
@@ -1,26 +1,23 @@
 CREATE TABLE IF NOT EXISTS musicas(
 	id INTEGER PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(40) NOT NULL UNIQUE,
-    duracao TIME DEFAULT '00:00:00',
-    compositor VARCHAR(40) NOT NULL,
-    
-    CHECK (duracao >= '00:00:00')
+    duracao TIME DEFAULT '00:00:00' CHECK (duracao >= '00:00:00'),
+    compositor VARCHAR(40) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS apresentacoes(
 	id INTEGER PRIMARY KEY AUTO_INCREMENT,
     endereco VARCHAR(100) NOT NULL,
     localidade VARCHAR(40) NOT NULL,
-    horario TIME NOT NULL ,
-    
-    CHECK (horario >= '00:00:00'),
-    UNIQUE(endereco,horario,localidade)
+    horario TIME NOT NULL DEFAULT '00:00:00' CHECK (horario >= '00:00:00')
+		
 );
 
 CREATE TABLE IF NOT EXISTS cargos(
 	id INTEGER PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(40) NOT NULL UNIQUE,
     funcao VARCHAR(100) DEFAULT 'nao especificado'
+    
 );
 
 CREATE TABLE IF NOT EXISTS integrantes(
@@ -28,14 +25,11 @@ CREATE TABLE IF NOT EXISTS integrantes(
 	cpf VARCHAR(11) UNIQUE NOT NULL,
 	nome VARCHAR(45) NOT NULL,
 	idCargos INTEGER,
-    idade INTEGER NOT NULL,
+    idade INTEGER NOT NULL CHECK(idade > 4),
     sexo VARCHAR(20) NOT NULL,
     endereco VARCHAR(100) NOT NULL,
-    experiencia INTEGER NOT NULL,
+    experiencia INTEGER NOT NULL
     
-    FOREIGN KEY (idCargos) REFERENCES cargos(id),
-    
-    CHECK(idade > 4)
 );
 
 CREATE TABLE IF NOT EXISTS fornecedores(
@@ -43,63 +37,47 @@ CREATE TABLE IF NOT EXISTS fornecedores(
     cnpj VARCHAR(14) NOT NULL UNIQUE,
     nome VARCHAR(40) NOT NULL,
     endereco VARCHAR(100) UNIQUE
+    
 );
 
 CREATE TABLE IF NOT EXISTS instrumentos(
-	id INTEGER PRIMARY KEY,
+	id INTEGER PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(45) NOT NULL,
     condicao VARCHAR(20) NOT NULL,
-    idFornecedores INTEGER,
+    idFornecedores INTEGER
     
-    FOREIGN KEY (idFornecedores) REFERENCES fornecedores(id)
 );
 
 CREATE TABLE IF NOT EXISTS musicas_Instrumentos(
     idMusicas INTEGER,
     idInstrumentos INTEGER,
-    qntInstrumentos INTEGER NOT NULL,
+    qntInstrumentos INTEGER NOT NULL
     
-	FOREIGN KEY (idMusicas) REFERENCES musicas(id),
-    FOREIGN KEY (idInstrumentos) REFERENCES instrumentos(id),
-    
-    PRIMARY KEY(idMusicas, idInstrumentos)
 );
 
 CREATE TABLE IF NOT EXISTS musicas_Integrantes(
 	idIntegrantes INTEGER,
     idMusicas INTEGER,
-    proficiencia VARCHAR(45) NOT NULL,
+    proficiencia VARCHAR(45) NOT NULL
     
-	FOREIGN KEY (idMusicas) REFERENCES musicas(id),
-    FOREIGN KEY (idIntegrantes) REFERENCES integrantes(id),
-
-    PRIMARY KEY(idMusicas,idIntegrantes)
 );
 
 CREATE TABLE IF NOT EXISTS integrantes_Instrumentos(
     idIntegrantes INTEGER,
     idInstrumentos INTEGER,
-    proficiencia VARCHAR(45) NOT NULL,
+    proficiencia VARCHAR(45) NOT NULL
     
-    FOREIGN KEY (idIntegrantes) REFERENCES integrantes(id),
-    FOREIGN KEY (idInstrumentos) REFERENCES instrumentos(id),
-    
-    PRIMARY KEY(idInstrumentos,idIntegrantes)
 );
 
 CREATE TABLE IF NOT EXISTS apresentacoes_Detalhadas(
     idIntegrantes INTEGER,
     idApresentacoes INTEGER,
     idMusicas INTEGER,
-    idInstrumentos INTEGER,
-
-	FOREIGN KEY(idIntegrantes) REFERENCES integrantes(id),
-    FOREIGN KEY(idInstrumentos) REFERENCES instrumentos(id),
-    FOREIGN KEY (idMusicas) REFERENCES musicas(id),
-    FOREIGN KEY (idApresentacoes) REFERENCES apresentacoes(id),
-    
-    PRIMARY KEY(idApresentacoes,idMusicas,idInstrumentos,idIntegrantes)
+    idInstrumentos INTEGER
+	
 );
+
+
 
 
 
