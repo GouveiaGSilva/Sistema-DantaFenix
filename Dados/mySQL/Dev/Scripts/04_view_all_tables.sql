cfd25df43687044a49e420b0bdb91cab367e7326
Guilherme Gouveia <gouveiagsilva@Gmail.com>
2025-07-21 10:06:14 -0300

    FEAT: adiciona script - ver todas as tabelas

---
+++ Dados/mySQL/Dev/Scripts/04_view_all_tables.sql
@@ -0,0 +1,37 @@
+SELECT *
+FROM apresentacoes;
+
+SELECT *
+FROM apresentacoes_detalhadas;
+
+SELECT *
+FROM cargos;
+
+SELECT *
+FROM condicoes;
+
+SELECT *
+FROM fornecedores;
+
+SELECT *
+FROM instrumentos;
+
+SELECT *
+FROM instrumentos_condicoes;
+
+SELECT *
+FROM integrantes;
+
+SELECT *
+FROM integrantes_instrumentos;
+
+SELECT *
+FROM musicas;
+
+SELECT *
+FROM musicas_instrumentos;
+
+SELECT *
+FROM musicas_integrantes;
+
+ 
